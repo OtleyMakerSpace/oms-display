b9c7eaf7f8e810530475e77ba4ca239047d5e924
David Frankland <david@dmf-software.co.uk>
2022-12-07 23:16:19 +0000

    remove unused imports

--- OMS_slide_show.py
+++ OMS_slide_show.py
@@ -8,9 +8,6 @@ import os
 import time
 import datetime
 from tkinter import *
-import requests
-import json
-from oauth2client.service_account import ServiceAccountCredentials
 
 on_hours = ['08','09','10','11','12','13','14','15','16','17','18','19','20','21']
 slides = []
