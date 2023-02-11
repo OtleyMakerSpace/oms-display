9007c4e1f0f6ef3caa2091bc438444ba28a3a5ca
David Frankland <david@dmf-software.co.uk>
2023-02-11 21:36:08 +0000

    add logging

--- OMS_slide_show.py
+++ OMS_slide_show.py
@@ -5,20 +5,34 @@ import time
 import datetime
 from tkinter import *
 import configparser
+import logging
+import logging.config
+
+# setup logging
+logging.config.fileConfig("logging.conf")
+logger = logging.getLogger()
+logger.info("starting up")
 
 # read settings from config file
+logging.info("reading config settngs")
 config = configparser.ConfigParser()
 config.read("settings.ini")
 settings = config["settings"]
 enable_blanking = settings.getboolean("enable-blanking", True)
+logging.debug(f"enable_blanking = {enable_blanking}")
 slide_time = settings.getint("slide-time", 10)
+logging.debug(f"slide_time = {slide_time}")
 start_hour = settings.getint("start-hour", 8)
 if start_hour < 0 or start_hour > 23:
     raise Exception("start hour must be in the range 0 to 23")
+logging.debug(f"start_hour = {start_hour}")
 end_hour = settings.getint("end-hour", 22)
 if end_hour < 0 or end_hour > 23:
     raise Exception("end hour must be in the range 0 to 23")
+logging.debug(f"end_hour = {end_hour}")
 images_folder = settings.get("images-folder")
+logging.debug(f"images_folder = {images_folder}")
+
 
 # globals
 blanked = False
@@ -66,7 +80,9 @@ def show_display():
 
 
 def reboot():
+    logger.info("rebooting soon")
     time.sleep(60)
+    logger.info("rebooting")
     os.system('sudo reboot')
 
 
@@ -90,27 +106,30 @@ def during_the_night():
 #### tkinter setup ####
 
 
-# creates the application window and makes it full screen
+# create the application window and makes it full screen
 root = Tk()
 root.attributes('-fullscreen', True)
 
-# sets up escape key to quit the program
+# setup escape key to quit the program
 root.bind("<Escape>", lambda event: root.destroy())
 
-# creates a frame
+# create a frame
 frame = Frame(root)
 frame.pack(fill=BOTH, expand=1)
 
-# creates a label for slides to be in
+# create a label for slides to be in
 label = Label(frame)
 label.pack()
 
-# loads the slide images
+# load the slide images
 slide_filenames = os.listdir(images_folder)
 slide_filenames.sort()
-slides = [PhotoImage(file=os.path.join(images_folder,f)) for f in slide_filenames]
+slides = [PhotoImage(file=os.path.join(images_folder, f))
+          for f in slide_filenames]
+logging.info(f"loaded {len(slides)} slides")
 
-# loads the black slide
+
+# load the black slide
 slide_black = PhotoImage(file='slide_black.png')
 
 #### main programme ####
