c039e7e32fc604b7a196afd24d16ccf42c569bb0
David Frankland <david@dmf-software.co.uk>
2025-08-22 17:11:39 +0100

    add more transitions

--- OMS_slide_show.py
+++ OMS_slide_show.py
@@ -164,7 +164,7 @@ slides = today_slides()
 gl_helper = glhelper.GlHelper(slides)
 
 # get list of transitions
-transitions = get_transitions("transitions")
+transitions = get_transitions("transitions-test")
 
 # show the slides during the day
 during_the_day(slides, transitions)
